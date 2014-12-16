b90698a0cbcb2a651a1f3f607932ef71311023bf
Asaf Manassen <asafma@Asafs-MBP.haifa.ibm.com>
2014-12-16 13:27:06 +0200

    generated files on 2014-12-16 13:27:06

--- update_to_server.sh
+++
@@ -1,6 +0,0 @@
-#git tag -a 0.1 -m 'my version 0.1 $(date)'
-#git push --tags
-git add --all .
-git commit -m "generated files on `date +'%Y-%m-%d %H:%M:%S'`";
-git push origin master
-#pod repo update
